867ea2c09faf3da5ffd57904f6b4b56ce7661770
Maxime Bruguet <maxime.bruguet@epitech.eu>
2018-11-18 16:46:47 +0100

    [FEATURE] V1 DONE

--- script.py
+++ script.py
@@ -1,17 +1,52 @@
 #!/usr/bin/python3
 
-from datetime import datetime
+from datetime import datetime, date
 import sys
+import os
 import requests
 import json
 
+import sendgrid
+from sendgrid.helpers.mail import *
+
 def usage():
-    print ("USAGE : " + sys.argv[0] + " <RRCODE CITY FROM (FRPAR, FRLIL, etc.)> <RRCODE CITY TO> <DATE YYYY-mm-DD>", file=sys.stdout)
+    print ("USAGE : " + sys.argv[0] + " <RRCODE CITY FROM (FRPAR, FRLIL, etc.)> <RRCODE CITY TO> <DATE (YYYY-mm-DD)>", file=sys.stdout)
     sys.exit(1)
 
-def sendEmail():
-    # Send an email with the results and timestamps
-    pass
+def customEmailBody(results):
+    dateFormatted = datetime.strptime(sys.argv[3], "%Y-%m-%d")
+    body = "Hello, these are the results of your search\n\n"
+    body += "At " + datetime.strftime(datetime.utcnow(), "%H:%M:%S - %d/%m/%Y") + "\n\n"
+    body += "From: " + sys.argv[1] + " - " + results[0]['origin']['cityLabel'] + "\n\n"
+    body += "To: " + sys.argv[2] + " - " + results[0]['dest']['cityLabel'] +"\n\n"
+    body += "Date: " + datetime.strftime(dateFormatted, "%d/%m/%Y") + "\n\n"
+    body += str(len(results)) + " direct train(s) found.\n\n"
+    for trip in results:
+        depHours = datetime.strptime(trip['departure'], "%Y-%m-%dT%H:%M")
+        arrHours = datetime.strptime(trip['arrival'], "%Y-%m-%dT%H:%M")
+        body += "TRAIN NUMBER " + trip['trainNumber'] + "\n\n"
+        body += "FROM " + trip['origin']['label'] + " in " + trip['origin']['cityLabel']
+        body += " AT " + datetime.strftime(depHours, "%H:%M") + "\n"
+        body += "TO " + trip['dest']['label'] + " in " + trip['dest']['cityLabel']
+        body += " AT " + datetime.strftime(arrHours, "%H:%M") + "\n\n"
+
+    return body
+
+def sendEmail(results):
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(os.environ.get('EMAIL_FROM'))
+    to_email = Email(os.environ.get('EMAIL_TO'))
+    subject = "Result of your TGV MAX search"
+    content = Content("text/plain", customEmailBody(results))
+    mail = Mail(from_email, subject, to_email, content)
+    try :
+        response = sg.client.mail.send.post(request_body=mail.get())
+    except:
+        print("The email couldn't be sent, please check your environment and try again later.", file=sys.stderr)    
+        sys.exit(1)
+    print(customEmailBody(results))
+    print("An email was sent to " + os.environ.get('EMAIL_TO'))
+    sys.exit()
 
 def parseAndFilterTrain(jsonPayload, fromHours, toHours):
     # Parse JSON and filter all the trains
@@ -24,8 +59,7 @@ def parseAndFilterTrain(jsonPayload, fromHours, toHours):
         filteredTrip['dest'] = trip['segments'][0]['destination']
         filteredTrip['trainNumber'] = trip['segments'][0]['trainNumber']
         parsedArray.append(filteredTrip)
-        print(filteredTrip)
-
+    sendEmail(sorted(parsedArray, key=lambda x: x['departure'], reverse=False))
 
 
 def getAvailableTrain(fromCode, toCode, date):
@@ -34,23 +68,18 @@ def getAvailableTrain(fromCode, toCode, date):
     try:
         r.raise_for_status()
     except:
-        print("The request failed - with code %d", r.status_code, file=sys.stdout)
+        print("The request failed - with code", r.status_code, file=sys.stdout)
         sys.exit(1)
 
     parseAndFilterTrain(r.json(), '', '')
 
 def main():
-    # Take few args : from (RRCODE), to (RRCODE), DATE (YYYY-MM-DD - less than 30 days ahead), FROM HOURS (24h/optional), TO HOURS (24h/optional)
     try:
         rrcodeFrom = sys.argv[1]
         rrcodeTo = sys.argv[2]
-        tripDate = datetime.strptime(sys.argv[3], "%Y-%m-%d")
+        datetime.strptime(sys.argv[3], "%Y-%m-%d")
     except:
         usage()
-
-    print(rrcodeFrom)
-    print(rrcodeTo)
-    print(tripDate)
 
     getAvailableTrain(rrcodeFrom, rrcodeTo, sys.argv[3])
 
